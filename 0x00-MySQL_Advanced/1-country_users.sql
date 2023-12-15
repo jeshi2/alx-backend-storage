11eb7330567da3a948d3a20b66d27bf8099c6a2d
jeshi2 <jamesvean19@gmail.com>
2023-12-15 21:49:39 +0300

    mysql advance

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,11 +1,7 @@
 -- Create the users table with specified attributes if not exists
 CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
-    country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
+    country ENUM('US', 'CO', 'TN') NOT NULL
 );
-
--- Add the country column to the existing table if not exists
-ALTER TABLE users
-ADD COLUMN IF NOT EXISTS country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL;
