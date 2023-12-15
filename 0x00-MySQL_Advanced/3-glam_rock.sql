10e1fc042747d58fbda1e289adfa6e0062c5ea92
jeshi2 <jamesvean19@gmail.com>
2023-12-15 21:56:50 +0300

    mysql advance

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,18 +1,10 @@
--- Create a temporary table to store bands with Glam rock as their main style
-CREATE TEMPORARY TABLE IF NOT EXISTS temp_glam_rock_bands AS
+-- List Glam rock bands ranked by their longevity
 SELECT
     band_name,
     YEAR('2022-01-01') - CAST(SUBSTRING_INDEX(lifespan, ' - ', -1) AS SIGNED) AS lifespan
 FROM
     metal_bands
 WHERE
-    main_style = 'Glam rock';
-
--- List Glam rock bands ranked by their longevity
-SELECT
-    band_name,
-    lifespan
-FROM
-    temp_glam_rock_bands
+    main_style = 'Glam rock'
 ORDER BY
     lifespan DESC;
