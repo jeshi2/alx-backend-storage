4e4b15f3c22559b493741a3cae04361564a71e5e
jeshi2 <jamesvean19@gmail.com>
2023-12-15 21:57:44 +0300

    mysql advance

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,10 +1,7 @@
--- List Glam rock bands ranked by their longevity
-SELECT
-    band_name,
-    YEAR('2022-01-01') - CAST(SUBSTRING_INDEX(lifespan, ' - ', -1) AS SIGNED) AS lifespan
-FROM
-    metal_bands
-WHERE
-    main_style = 'Glam rock'
-ORDER BY
-    lifespan DESC;
+-- Lists all bands with Glam rock as their main style, ranked by their longevity
+SELECT band_name, (IFNULL(split, 2022) - formed)
+AS lifespan 
+FROM metal_bands 
+WHERE style 
+LIKE '%Glam rock%' 
+ORDER BY lifespan DESC;
