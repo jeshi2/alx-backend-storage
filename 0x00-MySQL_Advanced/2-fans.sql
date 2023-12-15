babc407457641c4bec85a15a78dca012242875b7
jeshi2 <jamesvean19@gmail.com>
2023-12-15 21:45:39 +0300

    mysql advance

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,8 +1,10 @@
--- Import the metal_bands table dump
-SOURCE metal_bands.sql;
-
--- Rank country origins of bands by the number of non-unique fans
+-- Create a temporary table to store the aggregated fan counts for each origin
+CREATE TEMPORARY TABLE IF NOT EXISTS temp_fans_count AS
 SELECT origin, COUNT(*) AS nb_fans
 FROM metal_bands
-GROUP BY origin
+GROUP BY origin;
+
+-- Rank the country origins based on the number of (non-unique) fans
+SELECT origin, nb_fans
+FROM temp_fans_count
 ORDER BY nb_fans DESC;
