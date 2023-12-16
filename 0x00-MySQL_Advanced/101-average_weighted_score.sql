7a10edba2202900735a5f4bf46d3f12fbcbe1277
jeshi2 <jamesvean19@gmail.com>
2023-12-16 23:21:21 +0300

    mysql advance

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -3,12 +3,27 @@ DELIMITER //
 DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
-    DUPDATE users set average_score = (SELECT
-    SUM(corrections.score * projects.weight) / SUM(projects.weight)
-    FROM corrections
-    INNER JOIN projects
-    ON projects.id = corrections.project_id
-    where corrections.user_id = users.id);
+    DECLARE done INT DEFAULT FALSE;
+    DECLARE user_id_val INT;
+    DECLARE cur CURSOR FOR SELECT id FROM users;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+
+    -- Open cursor for users
+    OPEN cur;
+
+    -- Loop through each user and compute average weighted score
+    read_loop: LOOP
+        FETCH cur INTO user_id_val;
+        IF done THEN
+            LEAVE read_loop;
+        END IF;
+
+        -- Call the individual user computation procedure
+        CALL ComputeAverageWeightedScoreForUser(user_id_val);
+    END LOOP;
+
+    -- Close the cursor
+    CLOSE cur;
 END //
 
 DELIMITER ;
